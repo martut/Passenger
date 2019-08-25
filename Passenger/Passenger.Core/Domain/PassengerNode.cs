8b0708e17bb7a86eb075dbb473bf9983c3e96df8
martut <martut2762@gmail.com>
2019-08-25 17:43:55 +0200

    First Models, UserService, UserController

---
+++ Passenger/Passenger.Core/Domain/PassengerNode.cs
@@ -0,0 +1,9 @@
+namespace Passenger.Core.Domain
+{
+    public class PassengerNode
+    {
+        public Node Node { get; protected set; }
+        public Passenger Passenger { get; protected set; }
+        
+    }
+}
---
+++ Passenger/Passenger.Infrastructure/Services/IUserService.cs
@@ -0,0 +1,12 @@
+using Passenger.Infrastructure.DTO;
+
+namespace Passenger.Infrastructure.Services
+{
+    public interface IUserService
+    {
+        UserDto Get(string email);
+        void Register(string email, string username, string password);
+        
+        
+    }
+}
